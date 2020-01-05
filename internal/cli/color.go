12c412adaba5d7fe606748efbb6941b5cd211711
Breno Martinusso <martinusso@gmail.com>
2020-01-04 22:41:56 -0300

    remove unused colors

--- internal/cli/color.go
+++ internal/cli/color.go
@@ -2,16 +2,8 @@ package cli
 
 import "fmt"
 
-func Cyan(s string) string {
-	return fmt.Sprintf("\036[1;32m%s\036[0m", s)
-}
-
 func Green(s string) string {
 	return fmt.Sprintf("\033[1;32m%s\033[0m", s)
-}
-
-func Magenta(s string) string {
-	return fmt.Sprintf("\035[1;31m%s\035[0m", s)
 }
 
 func Red(s string) string {
